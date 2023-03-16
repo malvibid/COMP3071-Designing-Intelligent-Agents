9fb38712989f8bcdcbeefba80f9143b4bbbdc671
Malvi Bid <bidmalvi@gmail.com>
2023-03-16 18:53:39 +0800

    Task 5

--- lab_6/parserDemo.py
+++ lab_6/parserDemo.py
@@ -1,5 +1,5 @@
 import nltk
-from stat_parser import Parser
+from pyStatParser.stat_parser import Parser
 from pprint import pprint
 
 parser = Parser()
@@ -7,3 +7,13 @@ sentence = "My street is Triumph Road"
 t = parser.parse(sentence)
 t.draw()
 pprint(t)
+
+print("#######################")
+for st in t.subtrees():
+    print(st.label())
+    if st.label() == "VP":
+        vpTree = st
+for st in vpTree.subtrees():
+    if st.label() == "NP":
+        npTree = st
+print(" ".join(npTree.leaves()))
