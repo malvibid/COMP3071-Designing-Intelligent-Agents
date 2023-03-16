38bff1ad667a724e8b1e3facbbe47cebb7942103
Malvi Bid <bidmalvi@gmail.com>
2023-03-16 18:44:53 +0800

    Task 3

--- lab_6/chatbot.py
+++ lab_6/chatbot.py
@@ -40,8 +40,32 @@ while active:
         currentQuery = (person, fact, filterNoun[0])
         knowledge.add(currentQuery)
 
+        # Task 3: optional exercise
+        if fact == "want to buy" and filterNoun[0] == "book":
+            knowledge.add(("person1", "book title", "?"))
+            knowledge.add(("person1", "book author", "?"))
+
+        if fact == "want to buy" and filterNoun[0] == "pen":
+            knowledge.add(("person1", "pen colour", "?"))
+
     else:
         question = "How can I help you? "
         helpRequest = input(question)
-        # to fill in - process reply
+        # process reply
+        tokens = nltk.word_tokenize(helpRequest)
+        print("Tokens: \n" + str(tokens) + "\n")
+        taggedTokens = nltk.pos_tag(tokens)
+        print("Tagged tokens: \n" + str(taggedTokens) + "\n")
+
+        # get verb
+        filterVerb = [word for (word, pos)
+                      in taggedTokens if pos.startswith("VB")]
+        print("Verb: \n" + str(filterVerb) + "\n")
+
+        if any(item in ["buy", "order", "purchase"] for item in filterVerb):
+            # if "buy" in filterVerb:
+            knowledge.add(("person1", "want to buy", "?"))
+        else:
+            print("I cannot help you with that. Can I help you to buy something?")
+
     print()
