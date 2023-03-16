efcf1a1af88866a7efd66a42cae4a1d024d327eb
Malvi Bid <bidmalvi@gmail.com>
2023-03-16 18:35:04 +0800

    Task 1

--- lab_6/chatbot.py
+++ lab_6/chatbot.py
@@ -1,21 +1,32 @@
 from pprint import pprint
+import random
 
-knowledge = { ("person1", "name", "?"), \
-              ("person1", "town", "?"),
-              ("person1", "street", "?") }
+knowledge = {("person1", "name", "?"),
+             ("person1", "town", "?"),
+             ("person1", "street", "?")}
 
 active = True
 while active:
-    unknowns = { (person,fact,value) for (person,fact,value) \
-                 in knowledge if value=="?" }
+    unknowns = {(person, fact, value) for (person, fact, value)
+                in knowledge if value == "?"}
     print("UNKNOWN:")
     pprint(unknowns)
     print("KNOWN:")
     pprint(knowledge - unknowns)
-    if unknowns: #is non-empty
-        question = "## to fill in ## "
+    if unknowns:  # is non-empty
+        person, fact, value = random.choice(list(unknowns))
+        question = f"What is your {fact}?"
+
+        # remove current query from knowledge
+        currentQuery = (person, fact, value)
+        knowledge.remove(currentQuery)
+
         reply = input(question)
-        # to fill in - process reply
+
+        #  process reply
+        currentQuery = (person, fact, reply)
+        knowledge.add(currentQuery)
+
     else:
         question = "How can I help you? "
         helpRequest = input(question)
